b7cf5b657179d84fdff94496e7ca279ec0e98674
Stephen von Takach <steve@cotag.me>
2014-09-02 14:04:48 +1000

    initial structure

---
+++ angular-named-toggle.js
@@ -0,0 +1,11 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('coNamedToggle', [])
+        .directive('range', [
+            function () {
+
+            }
+        ]);
+
+}(this.angular));
