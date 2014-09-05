bf1adf9468afd056343665aa8c7705131cd61ea8
Stephen von Takach <steve@cotag.me>
2014-09-05 11:36:42 +1000

    Add iOS toggle to the library
    Mixin directly to the ng module

--- angular-named-toggle.js
+++ angular-named-toggle.js
@@ -5,7 +5,20 @@
     // https://github.com/nostalgiaz/bootstrap-switch
     // https://github.com/cgarvis/angular-toggle-switch
 
-    angular.module('coToggle', ['ng'])
+    var toggle = function ($scope) {
+            $scope.toggle = function toggle() {
+                if (!$scope.disabled) {
+                    if ($scope.model === $scope.trueValue) {
+                        $scope.model = $scope.falseValue;
+                    } else {
+                        $scope.model = $scope.trueValue;
+                    }
+                }
+                $scope.onChange();
+            };
+        };
+
+    angular.module('ng')
     .directive('toggleSwitch', function () {
         return {
             restrict: 'EA',
@@ -22,25 +35,14 @@
             },
             template:
                 '<div class="co-toggle" ng-click="toggle()" ng-class="{ disabled: disabled }">' +
-                    '<div class="switch-animate" ng-class="{active: model === trueValue}">' +
+                    '<div class="switch-animate" ng-class="{coActive: model === trueValue}">' +
                         '<span class="switch-left">{{onLabel}}</span>' +
                         '<span class="knob">{{knobLabel}}</span>' +
                         '<span class="switch-right">{{offLabel}}</span>' +
                     '</div>' +
                     '<span class="switch-min">{{largeText}}</span>' +
                 '</div>',
-            controller: function ($scope) {
-                $scope.toggle = function toggle() {
-                    if (!$scope.disabled) {
-                        if ($scope.model === $scope.trueValue) {
-                            $scope.model = $scope.falseValue;
-                        } else {
-                            $scope.model = $scope.trueValue;
-                        }
-                    }
-                    $scope.onChange();
-                };
-            },
+            controller: toggle,
             link: function(scope) {
                 scope.onLabel = scope.onLabel || "On";
                 scope.offLabel = scope.offLabel || "Off";
@@ -53,6 +55,26 @@
                     ].sort(function (a, b) { return b.length - a.length; })[0];
 
                 scope.largeText = longest + longest + 'buffer';
+            }
+        };
+    })
+
+    .directive('iosToggle', function () {
+        return {
+            template: '<div class="ios-toggle" ng-click="toggle()" ng-class="{coActive: model === trueValue, disabled: disabled}"></div>',
+            replace: true,
+            restrict: 'EA',
+            controller: toggle,
+            scope: {
+                model: '=?',
+                disabled: '=?',
+                trueValue: '=?',
+                falseValue: '=?',
+                onChange: '&'
+            },
+            link: function ($scope, $element) {
+                scope.trueValue = scope.trueValue || true;
+                scope.falseValue = scope.falseValue || false;
             }
         };
     });
